a5723a62747f31b3e660a69f001fec38c5ba8cca
avinal <185067@nith.ac.in>
2020-10-29 18:02:16 +0530

    authantication corrected

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config user.name "GitHub Actions"
+
 # Clone the target repo
 clone_repo_url="https://github.com/${GITHUB_REPOSITORY}.git"
 git clone --single-branch --branch "${INPUT_BUILD_FROM}" "${clone_repo_url}"
@@ -31,11 +34,8 @@ make_command=(${INPUT_MAKE_COMMAND})
 "${make_command[@]}"
 
 remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config user.name "${GITHUB_ACTOR}"
-echo "Config added"
 
 # Deploy Pages
-ghp-import -m "GitHub Pages Updated" -b "${INPUT_PAGES_BRANCH}" "${INPUT_DOCS_FOLDER}"
+ghp-import -m "GitHub Pages Updated" -r "${remote_repo}" -b "${INPUT_PAGES_BRANCH}" "${INPUT_DOCS_FOLDER}" 
 git push -f origin "${INPUT_PAGES_BRANCH}"
 echo "Page Deployment Successfull"
