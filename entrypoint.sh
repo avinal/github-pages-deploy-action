d7c2ead8fb4189149a82ec94ed44f9081be64650
avinal <185067@nith.ac.in>
2020-10-29 17:58:17 +0530

    name corrected

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,8 @@ make_command=(${INPUT_MAKE_COMMAND})
 
 remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config user.name "GitHub Actions"
+git config user.name "${GITHUB_ACTOR}"
+echo "Config added"
 
 # Deploy Pages
 ghp-import -m "GitHub Pages Updated" -b "${INPUT_PAGES_BRANCH}" "${INPUT_DOCS_FOLDER}"
